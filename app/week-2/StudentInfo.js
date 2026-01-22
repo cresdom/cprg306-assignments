dc852fb6fcac70e6eff46c216c507f0e34a64d55
cresdom <domcres807@icloud.com>
2026-01-22 11:09:24 -0700

    added StudentInfo function

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <header>
+            <h1>Student Name: Crescia Dominguez</h1>
+                <nav>
+                    <div>
+                        <ul>
+                        <Link href="https://github.com/cresdom/cprg306-assignments">Click me for Github link!</Link>
+                        </ul>
+                    </div>
+                </nav>
+        </header>
+    )
+}
