83632e0674c55da59122134c7e17df608c6de109
cresdom <domcres807@icloud.com>
2026-01-22 20:51:00 -0700

    improved html semantics

--- app/page.js
+++ app/page.js
@@ -4,10 +4,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>CPRG 306: Web Development 2 - Assignments</header>
-      <nav>
-        <Link href="week-2">Click me for Week 2</Link>
-      </nav>
+      <header>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      </header>
+      <main>
+        <nav>
+          <Link href="week-2">Click me for Week 2</Link>
+        </nav>
+      </main>
     </>
   );
 }
