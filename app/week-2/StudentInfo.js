f303ffab73b05977e1ad2c2746b48774e4b9619e
cresdom <domcres807@icloud.com>
2026-01-22 21:11:52 -0700

    moved header to page.js and fixed indentation

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 export default function StudentInfo() {
     return (
-        <header>
-            <h1>Student Name: Crescia Dominguez</h1>
-                <nav>
-                    <ul>
-                        <li>
-                    <Link href="https://github.com/cresdom/cprg306-assignments">Click me for Github link!</Link>
-                        </li>
-                    </ul>
-                </nav>
-        </header>
-    )
+<>
+    <h1>Student Name: Crescia Dominguez</h1>
+    <nav>
+        <ul>
+            <li>
+                <Link href="https://github.com/cresdom/cprg306-assignments">Click me for Github link!</Link>
+            </li>
+        </ul>
+    </nav>
+</>
+)
 }
