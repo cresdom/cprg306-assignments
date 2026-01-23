9555457e0d9f3234892e9fe25e3a76fcde30bb59
cresdom <domcres807@icloud.com>
2026-01-22 20:50:08 -0700

    improved html semantics

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -4,11 +4,11 @@ export default function StudentInfo() {
         <header>
             <h1>Student Name: Crescia Dominguez</h1>
                 <nav>
-                    <div>
-                        <ul>
-                        <Link href="https://github.com/cresdom/cprg306-assignments">Click me for Github link!</Link>
-                        </ul>
-                    </div>
+                    <ul>
+                        <li>
+                    <Link href="https://github.com/cresdom/cprg306-assignments">Click me for Github link!</Link>
+                        </li>
+                    </ul>
                 </nav>
         </header>
     )
