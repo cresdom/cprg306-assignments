95d444d9029e3c068842b37f5e9bbd3e983c7508
cresdom <domcres807@icloud.com>
2026-01-30 21:54:11 -0700

    Update GroceryItem.js

--- app/components/GroceryItem.js
+++ app/components/GroceryItem.js
@@ -1,9 +1,7 @@
 export default function Item(props) {
     return (
         <li className="bg-white border border-purple-700 rounded w-1/3 mx-auto mb-2 p-2">
-        <p className="font-bold">
-        {props.name}
-        </p>
+        <p className="font-bold">{props.name}</p>
 
         <p className="text-sm text-gray-600">Quantity: {props.quantity}</p>
 
