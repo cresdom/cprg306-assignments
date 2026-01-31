8a422bae22ab0ebe2402ee17188b766733a4eab1
cresdom <domcres807@icloud.com>
2026-01-30 20:57:28 -0700

    made landing page prettier

--- app/page.js
+++ app/page.js
@@ -2,18 +2,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <header>
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-t from-purple-400 to-fuchsia-100">
+      <header className="mb-8 text-center">
+        <h1 className="text-3xl font-bold mb-2 text-purple-900">CPRG 306: Web Development 2</h1>
+        <p>Assignments - Crescia D.</p>
       </header>
-      <main>
-        <nav>
-          <Link href="week-2">Click me for Week 2</Link>
-        </nav>
-        <nav>
-          <Link href="week-3">Click me for Week 3</Link>
-        </nav>
+
+      <main className="space-y-4">
+        <div className="bg-white px-6 py-4 rounded shadow">
+          <Link href="week-2" className="text-purple-600 font-light hover:underline">Week 2 Assignment</Link>
+        </div>
+
+        <div className="bg-white px-6 py-4 rounded shadow">
+          <Link href="week-3" className="text-purple-600 font-light hover:underline">Week 3 Assignment</Link>
+        </div>
       </main>
-    </>
+    </div>
+
   );
 }
