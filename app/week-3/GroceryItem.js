1d6f077fe0c980864de4499bd38c7330ae4b21a6
cresdom <domcres807@icloud.com>
2026-01-30 21:26:44 -0700

    commented out example of how deconstructed would look like

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -15,3 +15,15 @@ export default function Item(props) {
     </li>
     );
 }
+
+// if deconstructed it would look like this:
+//
+// export default function Item({ name, quantity, category }) {
+// return (
+//     <li className="bg-white border border-purple-700 rounded w-1/3 mx-auto mb-2 p-2">
+//     <p className="font-bold">{name}</p>
+//     <p className="text-sm text-gray-600">Quantity: {quantity}</p>
+//     <p className="text-sm text-gray-600">Category: {category}</p>
+//     </li>
+// );
+// }
