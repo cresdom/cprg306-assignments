644f0d23be433069a8dec9fb469b98a75dfb288f
cresdom <domcres807@icloud.com>
2026-01-30 19:29:33 -0700

    added page.js for week3

---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import GroceryItemList from "./GroceryItemList";
+
+export default function Page() {
+return (
+<main>
+    <h1 className="text-4xl font-bold text-purple-400 mb-2 mt-2 ml-3">Shopping List</h1>
+    <GroceryItemList/>
+</main>
+);
+}
+
