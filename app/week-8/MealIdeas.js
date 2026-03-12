c7b6dd17312293643da0f6b92370b8101ee0cc88
cresdom <domcres807@icloud.com>
2026-03-12 10:55:37 -0700

    addded loading and error handling to MealIdeas

--- app/week-8/MealIdeas.js
+++ app/week-8/MealIdeas.js
@@ -5,17 +5,40 @@ import { useEffect, useState } from "react";
 async function fetchMealIdeas(ingredient) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
 
+    if (!response.ok) {
+        throw new Error("Failed to fetch meal ideas.");
+    }
+
     const data = await response.json();
     return data.meals || [];
 }
 
 export default function MealIdeas({ ingredient }) {
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
     async function loadMealIdeas() {
         if (!ingredient) {
             setMeals([]);
+            setError("");
             return;
+        }
+
+        try {
+            setLoading(true);
+            setError("");
+            const mealIdeas = await fetchMealIdeas(ingredient);
+            setMeals(mealIdeas);
+        }
+
+        catch (err) {
+            setMeals([]);
+            setError("Could not load meal ideas.");
+        }
+
+        finally {
+            setLoading(false);
         }
 
         const mealIdeas = await fetchMealIdeas(ingredient);
@@ -30,8 +53,12 @@ export default function MealIdeas({ ingredient }) {
 
             {!ingredient ? (
                 <p className="text-purple-800 dark:text-purple-300">Select an item to see meal ideas!</p>
+            ) : loading ? (
+                <p className="text-purple-800 dark:text-purple-300">Loading meal ideas....</p>
+            ) : error ? (
+                <p className="text-red-600 dark:text-red-400"> {error} </p>
             ) : meals.length === 0 ? (
-                <p className="text-purple-800 dark:text-purple-300">No meal ideas found for <strong>{ingredient}</strong>.</p>
+                <p className="text-purple-800 dark:text-purple-300">No meal ideas found for <strong>{ingredient}</strong></p>
             ) : (
                 <>
                     <p className="mb-3 text-purple-800 dark:text-purple-300">Here are some meal ideas using <strong>{ingredient}</strong>:</p>
