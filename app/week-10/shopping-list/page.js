57a3a070189044e8e1e269f49fb0be03c0b4df18
cresdom <domcres807@icloud.com>
2026-03-25 17:01:15 -0600

    added async function and useEffect hook

--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -1,17 +1,28 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GroceryItemForm from "./GroceryItemForm";
 import ItemList from "./ItemList";
 import MealIdeas from "./MealIdeas";
 import { useUserAuth } from "../../contexts/AuthContext";
-
+import { getItems, addItem } from "../_services/shopping-list-service";
 
 export default function Page() {
     const { user } = useUserAuth();
     const [items, setItems] = useState([]);
     const [selectedItemName, setSelectedItemName] = useState("");
+
+    async function loadItems() {
+        const items = await getItems(user.uid);
+        setItems(items);
+    }
+
+    useEffect(() => {
+        if (user) {
+            loadItems();
+        }
+    }, [user]);
 
     if (!user) {
         return (
@@ -28,9 +39,9 @@ export default function Page() {
 
     function handleItemSelect(item) {
         const cleanedName = item.name
-        .split(",")[0]
-        .trim()
-        .replace(/[^\p{L}\p{N}\s-]/gu,"");
+            .split(",")[0]
+            .trim()
+            .replace(/[^\p{L}\p{N}\s-]/gu, "");
 
         setSelectedItemName(cleanedName);
     }
@@ -38,18 +49,18 @@ export default function Page() {
     return (
         <main className="min-h-screen bg-linear-to-r from-violet-100 to-fuchsia-200 dark:from-slate-950 dark:to-slate-900 flex flex-col items-center gap-6 text-purple-950 dark:text-purple-100">
             <div className="w-full max-w-xl">
-            <Link href="/week-10" className="text-purple-700 hover:underline font-light dark:text-purple-300">← Back to Home</Link>
-        </div>
-
-        <h1 className="text-3xl font-bold text-purple-900 text-center mt-8 dark:text-purple-200">Shopping List</h1>
-
-        <div className="flex flex-col md:flex-row gap-10 items-start">
-            <div className="flex flex-col items-center">
-                <GroceryItemForm onAddItem={handleAddItem} />
-                <ItemList items={items} onItemSelect={handleItemSelect} />
+                <Link href="/week-10" className="text-purple-700 hover:underline font-light dark:text-purple-300">← Back to Home</Link>
             </div>
-            <MealIdeas ingredient={selectedItemName} />
-        </div>
-    </main>
+
+            <h1 className="text-3xl font-bold text-purple-900 text-center mt-8 dark:text-purple-200">Shopping List</h1>
+
+            <div className="flex flex-col md:flex-row gap-10 items-start">
+                <div className="flex flex-col items-center">
+                    <GroceryItemForm onAddItem={handleAddItem} />
+                    <ItemList items={items} onItemSelect={handleItemSelect} />
+                </div>
+                <MealIdeas ingredient={selectedItemName} />
+            </div>
+        </main>
     );
 }
